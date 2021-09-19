d9ecaf8dae912764c6b1fb653a7bf7d95aaeda1b
je0489 <je0489@naver.com>
2021-09-19 19:02:59 +0900

    Chore: initial commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,11 @@
-/* App Component */
-// props => 부모컴포넌트가 자식컴포넌트한테 data를 전달하는 것
+import React from 'react';
 
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie'
-
-class App extends Component {
-
-  state = {}
-
-  componentDidMount() {
-  	this._getMovies();
-  }
-
-  _getMovies = async () => {         
-  	const movies = await this._callApi()    
-  	this.setState({							
-  		movies    // movies : movies
-  	})
-  }                                        
-
-  _callApi = () => {
-  	return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
-  	.then(response => response.json())
-  	.then(json => json.data.movies)
-  	.catch(err => console.log(err))
-  }
-
-  _rendeMovies = () => {
-  	const movies = this.state.movies.map((movie) => {
-  		console.log(movie)
-  		return <Movie 
-  		title={movie.title_english} 
-  		poster={movie.medium_cover_image} 
-  		genres={movie.genres}
-  		synopsis={movie.synopsis}
-  		key={movie.id}
-  		/>
-     })
-  	return movies
-  }
-
-  render() {
-  	const { movies } = this.state;
-
-    return (
-      <div className={movies ? "App" : "App--loading"}>
-     	{this.state.movies ? this._rendeMovies() : 'Loading' }
-      </div>
-    );
-  }
+function App() {
+	return (
+		<div>
+			<h1> Hello </h1> 
+		</div>
+	);
 }
 
 export default App;
