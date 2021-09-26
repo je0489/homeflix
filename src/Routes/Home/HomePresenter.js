b8e5cbdc047d27efdecc3db774ad8b1fff769e10
je0489 <je0489@naver.com>
2021-09-27 01:03:58 +0900

    Feat: add Home presenter of Container-Presenter Pattern

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,51 @@
-export default () => "HOME";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+import ErrorMsg from "Components/ErrorMsg";
+
+const Container = styled.div`
+  padding: 20px;
+`;
+
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Playing">
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Playing">
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
+        </Section>
+      )}
+      {error && <ErrorMsg type="error" text={error} />}
+    </Container>
+  );
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  upcoming: PropTypes.array,
+  popular: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default HomePresenter;
