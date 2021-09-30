16bf713b29903a6ba4d9be44c4be9443c6952a22
je0489 <je0489@naver.com>
2021-09-30 23:50:28 +0900

    Feat: add react-helmet

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,44 +1,76 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 
 import Loader from "Components/Loader";
 import Section from "Components/Section";
+import Poster from "Components/Poster";
 import ErrorMsg from "Components/ErrorMsg";
 
 const Container = styled.div`
   padding: 20px;
 `;
 
-const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) =>
-  loading ? (
-    <Loader />
-  ) : (
-    <Container>
-      {nowPlaying && nowPlaying.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {upcoming && upcoming.length > 0 && (
-        <Section title="Upcoming Playing">
-          {upcoming.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Playing">
-          {popular.map((movie) => (
-            <span key={movie.id}>{movie.title}</span>
-          ))}
-        </Section>
-      )}
-      {error && <ErrorMsg type="error" text={error} />}
-    </Container>
-  );
+const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => (
+  <>
+    <Helmet>
+      <title>Home - Netflix</title>
+    </Helmet>
+    {loading ? (
+      <Loader />
+    ) : (
+      <Container>
+        {nowPlaying && nowPlaying.length > 0 && (
+          <Section title="Upcoming Playing">
+            {upcoming.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                genres={movie.genre_ids}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {upcoming && upcoming.length > 0 && (
+          <Section title="Upcoming Playing">
+            {upcoming.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                genres={movie.genre_ids}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Playing">
+            {popular.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imageUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                genres={movie.genre_ids}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <ErrorMsg type="error" text={error} />}
+      </Container>
+    )}
+  </>
+);
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
