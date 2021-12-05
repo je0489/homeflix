fcc46ae88621c66279b74b2c40ff6a918a120e1e
je0489 <je0489@naver.com>
2021-12-05 19:29:58 +0900

    Fix: Fix structure of header

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -11,8 +11,8 @@ const globalStyles = createGlobalStyle`
         color: inherit;
     }
     body {
-        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 15px;
+        font-family: sans-serif;
+        font-size: 16px;
         background-color: rgba(20, 20, 20, 1);
         color: white;
         padding-top: 60px;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 
+const logo_src = "logo__longer.png";
+const normal = 100,
+  bolder = 600;
+
 const Header = styled.header`
   position: fixed;
   display: flex;
@@ -9,28 +13,40 @@ const Header = styled.header`
   top: 0;
   left: 0;
   width: 100%;
-  height: 60px;
-  padding-top: 4px;
+  height: 3.75rem;
   padding-left: 20px;
-  font-size: 1.1rem;
+  font-size: 0.9rem;
   background-color: rgba(20, 20, 20, 0.8);
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
 `;
 
+const Main = styled.div`
+  padding: 0 1rem;
+  color: #e42414;
+  font-family: impact;
+  font-size: 1.8rem;
+
+  & img {
+    width: 8rem;
+  }
+`;
+
 const List = styled.ul`
-  display: flex;
+  display: inherit;
 `;
 
 const Item = styled.li`
-  /* width: 80px; */
-  height: 60px;
+  height: 3.75rem;
   text-align: center;
   text-transform: uppercase;
-  border-bottom: 5px solid
-    ${(props) => (props.isCurrent ? "#E50914" : "transparent")};
-  transition: border-bottom 0.5s ease-in-out;
+  font-weight: ${(props) => (props.isCurrent ? `${bolder}` : `${normal}`)};
   padding: 0 20px;
+
+  &:hover {
+    opacity: ${(props) => (props.isCurrent ? "inherit" : 0.6)};
+    transition: 0.4s ease-out;
+  }
 `;
 
 const SLink = styled(Link)`
@@ -42,12 +58,23 @@ const SLink = styled(Link)`
 
 export default withRouter(({ location: { pathname } }) => (
   <Header>
+    <Main>
+      <SLink to="/">
+        <img src={logo_src} alt="logo" />
+      </SLink>
+    </Main>
     <List>
       <Item isCurrent={pathname === "/"}>
-        <SLink to="/">Movies</SLink>
+        <SLink to="/"> 홈</SLink>
       </Item>
       <Item isCurrent={pathname === "/tv"}>
-        <SLink to="/tv">TV</SLink>
+        <SLink to="/tv">TV 프로그램</SLink>
+      </Item>
+      <Item isCurrent={pathname === "/movie"}>
+        <SLink to="/movie">영화</SLink>
+      </Item>
+      <Item isCurrent={pathname === "/trending"}>
+        <SLink to="/trending">NEW! 요즘 대세 콘텐츠</SLink>
       </Item>
       <Item isCurrent={pathname === "/search"}>
         <SLink to="/search">Search</SLink>
