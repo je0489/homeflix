5e45b399d7740d6b828f1b327cd458b21070367c
je0489 <je0489@naver.com>
2021-12-06 22:34:26 +0900

    Feat: Add movie and tranding router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -7,8 +7,10 @@ import {
 } from "react-router-dom";
 import Header from "./Header";
 import Home from "Routes/Home/index";
+import Movie from "Routes/Movie/index";
 import TV from "Routes/TV/index";
 import Search from "Routes/Search/index";
+import Tranding from "Routes/Tranding/index";
 import Detail from "Routes/Detail";
 
 export default () => (
@@ -17,7 +19,9 @@ export default () => (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
+      <Route path="/movie" exact component={Movie} />
       <Route path="/search" exact component={Search} />
+      <Route path="/trending" exact component={Tranding} />
       <Route path="/movie/:id" component={Detail} />
       <Route path="/show/:id" component={Detail} />
       <Redirect from="*" to="/" />
